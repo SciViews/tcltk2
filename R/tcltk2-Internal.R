d01c5a3619c5735c8b56fde9e0124e95c7bc932c
phgrosjean <phgrosjean@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-01-27 17:39:14 +0000

    In svMisv, TempEnv is renamed SciViews:TempEnv to avoid potential conflicts with other CRAN packages

--- R/tcltk2-Internal.R
+++ R/tcltk2-Internal.R
@@ -272,12 +272,12 @@
 
 .TempEnv <- function ()
 {
-    pos <-  match("TempEnv", search())
+    pos <-  match("SciViews:TempEnv", search())
     if (is.na(pos)) {  # Must create it
-        TempEnv <- list()
-        attach(TempEnv, pos = length(search()) - 1)
-        rm(TempEnv)
-        pos <- match("TempEnv", search())
+        `SciViews:TempEnv` <- list()
+        attach(`SciViews:TempEnv`, pos = length(search()) - 1)
+        rm(`SciViews:TempEnv`)
+        pos <- match("SciViews:TempEnv", search())
     }
     return(pos.to.env(pos))
 }
